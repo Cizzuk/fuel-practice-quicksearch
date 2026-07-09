bba1052f9b75d782a1ad194cf615d78a4229976f
Cizzuk <dropcat@cizzuk.net>
2026-07-09 01:54:26 +0000

    Model_UsersでDBクラスの関数をより活用するように修正

--- fuel/app/classes/model/users.php
+++ fuel/app/classes/model/users.php
@@ -22,14 +22,14 @@ class Model_Users extends \Model
     // ユーザーを追加
     public static function create_user($name, $password, $url_key, $default_engine_id = null)
     {
-        $sql = "INSERT INTO `users` (
-            name, password, url_key, default_engine_id, created_at)
-            VALUES (:name, :password, :url_key, :default_engine_id, NOW())";
-        \DB::query($sql)
-            ->bind('name', $name)
-            ->bind('password', $password)
-            ->bind('url_key', $url_key)
-            ->bind('default_engine_id', $default_engine_id)
+        \DB::insert('users')
+            ->set(array(
+                'name' => $name,
+                'password' => $password,
+                'url_key' => $url_key,
+                'default_engine_id' => $default_engine_id,
+                'created_at' => \DB::expr('NOW()'),
+            ))
             ->execute();
     }
 
@@ -38,9 +38,9 @@ class Model_Users extends \Model
     // nameで取得
     public static function get_user_by_name($name)
     {
-        $sql = "SELECT * FROM `users` WHERE name = :name";
-        return \DB::query($sql)
-            ->bind('name', $name)
+        return \DB::select('*')
+            ->from('users')
+            ->where('name', '=', $name)
             ->execute()
             ->current();
     }
@@ -48,9 +48,9 @@ class Model_Users extends \Model
     // url_keyで取得
     public static function get_user_by_url_key($url_key)
     {
-        $sql = "SELECT * FROM `users` WHERE url_key = :url_key";
-        return \DB::query($sql)
-            ->bind('url_key', $url_key)
+        return \DB::select('*')
+            ->from('users')
+            ->where('url_key', '=', $url_key)
             ->execute()
             ->current();
     }
@@ -58,9 +58,9 @@ class Model_Users extends \Model
     // idで取得
     public static function get_user_by_id($id)
     {
-        $sql = "SELECT * FROM `users` WHERE id = :id";
-        return \DB::query($sql)
-            ->bind('id', $id)
+        return \DB::select('*')
+            ->from('users')
+            ->where('id', '=', $id)
             ->execute()
             ->current();
     }
@@ -70,20 +70,18 @@ class Model_Users extends \Model
     // パスワードをidで更新
     public static function update_password($id, $new_password)
     {
-        $sql = "UPDATE `users` SET password = :password WHERE id = :id";
-        \DB::query($sql)
-            ->bind('password', $new_password)
-            ->bind('id', $id)
+        \DB::update('users')
+            ->set(array('password' => $new_password))
+            ->where('id', '=', $id)
             ->execute();
     }
 
     // デフォルトエンジンIDをidで更新
     public static function update_default_engine_id($id, $default_engine_id)
     {
-        $sql = "UPDATE `users` SET default_engine_id = :default_engine_id WHERE id = :id";
-        \DB::query($sql)
-            ->bind('default_engine_id', $default_engine_id)
-            ->bind('id', $id)
+        \DB::update('users')
+            ->set(array('default_engine_id' => $default_engine_id))
+            ->where('id', '=', $id)
             ->execute();
     }
 
@@ -92,9 +90,8 @@ class Model_Users extends \Model
     // ユーザーをidで削除
     public static function delete_user($id)
     {
-        $sql = "DELETE FROM `users` WHERE id = :id";
-        \DB::query($sql)
-            ->bind('id', $id)
+        \DB::delete('users')
+            ->where('id', '=', $id)
             ->execute();
     }
 }
